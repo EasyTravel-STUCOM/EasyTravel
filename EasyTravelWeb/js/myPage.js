91ba67b157e37097f15f101b41e97a0138217e9d
mullayos <afnam78@gmail.com>
2022-05-02 16:06:33 +0200

    Conectando BBDD con proyeto

--- EasyTravelWeb/js/myPage.js
+++ EasyTravelWeb/js/myPage.js
@@ -1,10 +1,10 @@
-$(".guardar").click(actualizarDatos);
-function actualizarDatos(){
+$(".guardar").click(function (){
     
     let nombre = $("#nombre").val();
     let apellido1 = $("#apellido1").val();
     let apellido2 = $("#apellido2").val();
 
+    console.log(nombre);
 
     $.ajax({
         type: "POST",
@@ -12,7 +12,18 @@ function actualizarDatos(){
         data: "nombre="+nombre+"apellido1="+apellido1+"apellido2="+apellido2+"changed="+false,
         dataType: "json",
         succes: function(respJSON){
-            console.log()
+            console.log(respJSON['user'])
         }
     })
+});
+
+
+function copyToClipBoard() {
+
+    var content = document.getElementById('textArea');
+    
+    content.select();
+    document.execCommand('copy');
+
+    alert("Copied!");
 }
--- EasyTravelWeb/php/MyPage.php
+++ EasyTravelWeb/php/MyPage.php
@@ -1,20 +1,79 @@
 <?php
-
-try{
-    $user = "root"; 
-    $password = " ";
+session_start();
+try {
+    $user = "root";
+    $password = "nintendo55";
     $dbName = "mysql:host=localhost;port=3306,dbname=EasyTravel";
     $PDO = new PDO($dbName, $user, $password);
-}catch(PDOException $e){
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
+
+$_SESSION['user']['name'] = "x";
+$_SESSION['user']['surname1'] = "x";
+$_SESSION['user']['surname2'] = "x";
 
 $nombre = $_POST['nombre'];
 $apellido1 = $_POST['apellido1'];
 $apellido2 = $_POST['apellido2'];
 
-if(!empty($apellido2)){
-    
+if (!empty($apellido2) && empty($nombre) && empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['surname2'] = $apellido2;
 }
+
+if (empty($apellido2) && empty($nombre) && !empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['surname1'] = $apellido1;
+}
+
+if (empty($apellido2) && !empty($nombre) && empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['name'] = $nombre;
+}
+
+if (empty($apellido2) && !empty($nombre) && !empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['name'] = $nombre;
+    $_SESSION['user']['surname1'] = $apellido1;
+}
+
+if (!empty($apellido2) && !empty($nombre) && empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['name'] = $nombre;
+    $_SESSION['user']['surname2'] = $apellido2;
+}
+
+if (!empty($apellido2) && empty($nombre) && !empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['surname1'] = $apellido1;
+    $_SESSION['user']['surname2'] = $apellido2;
+}
+
+if (!empty($apellido2) && !empty($nombre) && !empty($apellido1)) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+    $stmt->execute();
+    $_SESSION['user']['name'] = $nombre;
+    $_SESSION['user']['surname1'] = $apellido1;
+    $_SESSION['user']['surname2'] = $apellido2;
+}
+
+echo json_encode($_SESSION['user']);
 
 ?>
