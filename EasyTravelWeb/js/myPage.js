2ee6fb9e393bb9fabd1f4dc2b24dd440bb7c6482
mullayos <afnam78@gmail.com>
2022-05-02 17:52:22 +0200

    PHP hecho, falta añadir funcionaldiades extra

--- EasyTravelWeb/js/myPage.js
+++ EasyTravelWeb/js/myPage.js
@@ -1,27 +1,115 @@
-$(".guardar").click(function (){
-    
+$(".guardar").click(function () {
+
     let nombre = $("#nombre").val();
     let apellido1 = $("#apellido1").val();
     let apellido2 = $("#apellido2").val();
 
     console.log(nombre);
 
-    $.ajax({
-        type: "POST",
-        url: "../php/MyPage.php",
-        data: "nombre="+nombre+"apellido1="+apellido1+"apellido2="+apellido2+"changed="+false,
-        dataType: "json",
-        succes: function(respJSON){
-            console.log(respJSON['user'])
-        }
-    })
+    if (nombre.length != 0 && apellido1.length == 0 && apellido2.length == 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/EasyTravel/EasyTravelWeb/php/MyPage.php",
+            data: "nombre=" + nombre,
+            dataType: "json",
+            success: function (respJSON) {
+                if (respJSON.changed == true) {
+                    console.log("Datos actualizados");
+                } else {
+                    console.log("Error en la actualización de datos.");
+
+                }
+            }
+        })
+    }else if (nombre.length == 0 && apellido1.length != 0 && apellido2.length == 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/EasyTravel/EasyTravelWeb/php/MyPage.php",
+            data: "apellido1=" + apellido1,
+            dataType: "json",
+            success: function (respJSON) {
+                if (respJSON.changed == true) {
+                    console.log("Datos actualizados");
+                } else {
+                    console.log("Error en la actualización de datos.");
+
+                }
+            }
+        })
+
+    } else if (nombre.length == 0 && apellido1.length != 0 && apellido2.length != 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/EasyTravel/EasyTravelWeb/php/MyPage.php",
+            data: "apellido1=" + apellido1 + "&apellido2=" + apellido2,
+            dataType: "json",
+            success: function (respJSON) {
+                if (respJSON.changed == true) {
+                    console.log("Datos actualizados");
+                } else {
+                    console.log("Error en la actualización de datos.");
+
+                }
+            }
+        })
+
+    } else if (nombre.length != 0 && apellido1.length != 0 && apellido2.length != 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/EasyTravel/EasyTravelWeb/php/MyPage.php",
+            data: "nombre=" + nombre + "&apellido1=" + apellido1 + "&apellido2=" + apellido2,
+            dataType: "json",
+            success: function (respJSON) {
+                if (respJSON.changed == true) {
+                    console.log("Datos actualizados");
+                } else {
+                    console.log("Error en la actualización de datos.");
+
+                }
+            }
+        })
+
+    } else if (nombre.length != 0 && apellido1.length != 0 && apellido2.length == 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/EasyTravel/EasyTravelWeb/php/MyPage.php",
+            data: "nombre=" + nombre + "&apellido1=" + apellido1,
+            dataType: "json",
+            success: function (respJSON) {
+                if (respJSON.changed == true) {
+                    console.log("Datos actualizados");
+                } else {
+                    console.log("Error en la actualización de datos.");
+
+                }
+            }
+        })
+
+    } else if (nombre.length != 0 && apellido1.length == 0 && apellido2.length != 0) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/EasyTravel/EasyTravelWeb/php/MyPage.php",
+            data: "nombre=" + nombre + "&apellido2=" + apellido2,
+            dataType: "json",
+            success: function (respJSON) {
+                if (respJSON.changed == true) {
+                    console.log("Datos actualizados");
+                } else {
+                    console.log("Error en la actualización de datos.");
+
+                }
+            }
+        })
+    }
+
+
 });
 
 
 function copyToClipBoard() {
 
     var content = document.getElementById('textArea');
-    
+
     content.select();
     document.execCommand('copy');
 
--- EasyTravelWeb/php/MyPage.php
+++ EasyTravelWeb/php/MyPage.php
@@ -3,7 +3,7 @@ session_start();
 try {
     $user = "root";
     $password = "nintendo55";
-    $dbName = "mysql:host=localhost;port=3306,dbname=EasyTravel";
+    $dbName = "mysql:host=localhost;dbname=EasyTravel";
     $PDO = new PDO($dbName, $user, $password);
 } catch (PDOException $e) {
     echo $e->getMessage();
@@ -12,68 +12,107 @@ try {
 $_SESSION['user']['name'] = "x";
 $_SESSION['user']['surname1'] = "x";
 $_SESSION['user']['surname2'] = "x";
+$_SERVER['user']['changed'] = false;
 
-$nombre = $_POST['nombre'];
-$apellido1 = $_POST['apellido1'];
-$apellido2 = $_POST['apellido2'];
 
-if (!empty($apellido2) && empty($nombre) && empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+$_SESSION['user']['id'] = 1;
+
+
+
+if (isset($_POST['apellido2'])) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = :apellido2 WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('apellido2', $_POST['apellido2']);
     $stmt->execute();
-    $_SESSION['user']['surname2'] = $apellido2;
+    $_SESSION['user']['surname2'] = $_POST['apellido2'];
+    $_SESSION['user']['changed'] = true;
 }
 
-if (empty($apellido2) && empty($nombre) && !empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+if (isset($_POST['apellido1'])) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = :apellido1 WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('apellido1', $_POST['apellido1']);
     $stmt->execute();
-    $_SESSION['user']['surname1'] = $apellido1;
+    $_SESSION['user']['surname1'] = $_POST['apellido1'];
+    $_SESSION['user']['changed'] = true;
 }
 
-if (empty($apellido2) && !empty($nombre) && empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+if (isset($_POST['nombre'])) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = :nombre WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('nombre', $_POST['nombre']);
     $stmt->execute();
-    $_SESSION['user']['name'] = $nombre;
+    $_SESSION['user']['name'] = $_POST['nombre'];
+    $_SESSION['user']['changed'] = true;
 }
 
-if (empty($apellido2) && !empty($nombre) && !empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+if ((isset($_POST['nombre']) && isset($_POST['apellido1']))) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = :nombre WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('nombre', $_POST['nombre']);
     $stmt->execute();
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = :apellido1 WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('apellido1', $_POST['apellido1']);
     $stmt->execute();
-    $_SESSION['user']['name'] = $nombre;
-    $_SESSION['user']['surname1'] = $apellido1;
+
+    $_SESSION['user']['name'] = $_POST['nombre'];
+    $_SESSION['user']['surname1'] = $_POST['apellido1'];
+    $_SESSION['user']['changed'] = true;
 }
 
-if (!empty($apellido2) && !empty($nombre) && empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+if (isset($_POST['apellido2']) && isset($_POST['nombre'])) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = :nombre WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('nombre', $_POST['nombre']);
     $stmt->execute();
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = :apellido2 WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('apellido2', $_POST['apellido2']);
     $stmt->execute();
-    $_SESSION['user']['name'] = $nombre;
-    $_SESSION['user']['surname2'] = $apellido2;
+
+    $_SESSION['user']['name'] = $_POST['nombre'];
+    $_SESSION['user']['surname2'] = $_POST['apellido2'];
+    $_SESSION['user']['changed'] = true;
 }
 
-if (!empty($apellido2) && empty($nombre) && !empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+if (isset($_POST['apellido2']) && isset($_POST['apellido1'])) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = :apellido1 WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('apellido1', $_POST['apellido1']);
     $stmt->execute();
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = :apellido2 WHERE idUsuario = :id");
+    $stmt->bindValue('apellido2', $_POST['apellido2']);
+    $stmt->bindValue('id', $_SESSION['user']['id']);
     $stmt->execute();
-    $_SESSION['user']['surname1'] = $apellido1;
-    $_SESSION['user']['surname2'] = $apellido2;
+    $_SESSION['user']['surname1'] = $_POST['apellido1'];
+    $_SESSION['user']['surname2'] = $_POST['apellido2'];
+    $_SESSION['user']['changed'] = true;
 }
 
-if (!empty($apellido2) && !empty($nombre) && !empty($apellido1)) {
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = " . $apellido1 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+if (isset($_POST['apellido2']) && isset($_POST['nombre']) && isset($_POST['apellido1'])) {
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = :apellido2 WHERE idUsuario = :id");
+    $stmt->bindValue('apellido2', $_POST['apellido2']);
+    $stmt->bindValue('id', $_SESSION['user']['id']);
     $stmt->execute();
-    $stmt = $PDO->prepare("UPDATE Usuario SET apellido2 = " . $apellido2 . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+
+    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = :nombre WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('nombre', $_POST['nombre']);
     $stmt->execute();
-    $stmt = $PDO->prepare("UPDATE Usuario SET nombre = " . $nombre . " WHERE idUsuario = " . $_SESSION["user"]["id"]);
+
+    $stmt = $PDO->prepare("UPDATE Usuario SET apellido1 = :apellido1 WHERE idUsuario = :id");
+    $stmt->bindValue('id', $_SESSION['user']['id']);
+    $stmt->bindValue('apellido1', $_POST['apellido1']);
     $stmt->execute();
-    $_SESSION['user']['name'] = $nombre;
-    $_SESSION['user']['surname1'] = $apellido1;
-    $_SESSION['user']['surname2'] = $apellido2;
+
+    $_SESSION['user']['name'] = $_POST['nombre'];
+    $_SESSION['user']['surname1'] = $_POST['apellido1'];
+    $_SESSION['user']['surname2'] = $_POST['apellido2'];
+    $_SESSION['user']['changed'] = true;
 }
 
 echo json_encode($_SESSION['user']);
-
-?>
