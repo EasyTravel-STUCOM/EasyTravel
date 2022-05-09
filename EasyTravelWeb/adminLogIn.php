4b40a9fc2efc135427e1e46b99dc376b2ffbb5d8
ArcienegaCarlos <arvabussines@gmail.com>
2022-05-09 12:04:01 +0200

    creacion de la pagina de admin y la view

---
+++ EasyTravelWeb/adminLogIn.php
@@ -0,0 +1,61 @@
+<?php
+try {
+    $user = "root";
+    $password = "Supercarlos1";
+    $dataName = "mysql:host=localhost; port = 3306; dbname=easytravelst2122";
+    $dbh = new PDO($dataName, $user, $password);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+if(isset($_POST['logIn'])){
+    $username = $_POST['username']; 
+    $pwd = $_POST['pwd']; 
+    $logInTry = $dbh->prepare("SELECT * FROM usuario WHERE nombreUsuario = :username");
+    $logInTry->bindValue(":username", $username); 
+    $logInTry->execute(); 
+    $exist= $logInTry->fetchAll(PDO::FETCH_ASSOC);
+  
+    if(!empty($exist)){
+        if(password_verify($pwd,$exist[0]['userPassword']) && $exist[0]['rol']=="admin"){
+            header('Location: adminView.php');
+        }
+        if($exist[0]['rol']!="admin"){
+            echo "<h1>Este usuario no es administrador</h1>";
+        }
+        if(!password_verify($pwd,$exist[0]['userPassword'])){
+            echo "<h3>Usuario o contraseña incorrectos</h3>";
+        }
+    }else{
+        echo "<h1>Usuario o contraseña incorrectos </h1>";
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adminView.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form METHOD='POST'>
+        <label>Inserta tu nombre de usuario</label>
+        <input type="text" name="username" id="username">
+        <br>
+        <label>Inserta tu contraseña</label>
+        <br>
+        <input type="password" name="pwd" id="pwd">
+        <br>
+        <input type="submit" name='logIn'value="Entrar">
+    </form>
+</body>
+
+</html>
---
+++ EasyTravelWeb/adminView.php
@@ -0,0 +1,61 @@
+<?php 
+    try {
+        $user = "root";
+        $password = "Supercarlos1";
+        $dataName = "mysql:host=localhost; port = 3306; dbname=easytravelst2122";
+        $dbh = new PDO($dataName, $user, $password);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+    $logInTry = $dbh->prepare("SELECT * FROM usuario;");
+    $logInTry->execute(); 
+    $usuarios = $logInTry-> fetchAll(PDO::FETCH_ASSOC); 
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Lista de Usuarios: </h3>
+    <table>
+            <tr>
+                <th>id</th>
+                <th>Nombres</th>
+                <th>Apellido 1</th>
+                <th>Apellido 2</th>
+                <th>Fecha de nacimiento</th>
+                <th>Numero de telefono</th>
+                <th>Mail</th>
+                <th>Nombre de Usuario</th>
+                <th>Rol</th></th> 
+                <?php
+
+                        foreach( $usuarios as $clave => $row){
+                        $id = $row['idUsuario']; 
+                        echo"<tr>";
+                            echo"<td>".$row['idUsuario']."</td>";
+                            echo"<td>".$row['nombre']."</td>";
+                            echo"<td>".$row['apellido1']."</td>";
+                            echo"<td>".$row['apellido2']."</td>";
+                            echo"<td>".$row['fechaDeNacimiento']."</td>";
+                            echo"<td>".$row['telefono']."</td>";
+                            echo"<td>".$row['mail']."</td>";
+                            echo"<td>".$row['nombreUsuario']."</td>";
+                            echo"<td>".$row['rol']."</td>";
+                            echo"<td><a href='editUser.php?id=$id'>Editar</a></td>";
+                            echo"<td><a href='removeUser.php?id='$id'>Eliminar</a></td>";
+                        echo"</tr>";
+                        }
+
+                    ?>
+
+            <tr>
+        </table>
+</body>
+</html>
