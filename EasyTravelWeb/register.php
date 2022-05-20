7fa7505616a3732bd69339c656fc463ef9d2cf0c
ArcienegaCarlos <arvabussines@gmail.com>
2022-05-20 08:38:29 +0200

    esta completa la parte del registro

--- EasyTravelWeb/register.php
+++ EasyTravelWeb/register.php
@@ -1,35 +1,40 @@
-<?php 
-    try {
-        $user = "root";
-        $password = "Supercarlos1";
-        $dataName = "mysql:host=localhost; port = 3306; dbname=easytravelst2122";
-        $dbh = new PDO($dataName, $user, $password);
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-    }
+<?php
+try {
+    $user = "root";
+    $password = "Supercarlos1";
+    $dataName = "mysql:host=localhost; port = 3306; dbname=easytravelst2122";
+    $dbh = new PDO($dataName, $user, $password);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
-    $stmtComprMail = "SELECT * FROM usuario WHERE mail = :email"; 
-    $compMail = $dbh -> prepare($stmtComprMail); 
-    $compMail->bindValue(":email",$_POST['mail']);
+
+
+session_start();
+if (isset($_POST['nextButton'])) {
+    $stmtComprMail = "SELECT * FROM usuario WHERE mail = :email";
+    $compMail = $dbh->prepare($stmtComprMail);
+    $compMail->bindValue(":email", $_POST['mail']);
     $compMail->execute();
     $mails = $compMail->fetchAll(PDO::FETCH_ASSOC);
-
-    $mailRepetido; 
-    empty($mails)?$mailRepetido=true:$mailRepetido= false; 
-
-    session_start(); 
-    if(!isset($_SESSION) && isset($_POST['nextButton'])){
-        $_SESSION['userToAdd']['nombre'] = $_POST['name']; 
-        $_SESSION['userToAdd']['apellido1'] = $_POST['secondName']; 
-        $_SESSION['userToAdd']['apellido2'] = $_POST['thirdName']; 
-        $_SESSION['userToAdd']['fechaDeNacimiento'] = $_POST['birthday']; 
-        $_SESSION['userToAdd']['mail'] = $_POST['mail']; 
-
+    !empty($mails) ? $mailRepetido = true : $mailRepetido = false;
+    if(!$mailRepetido){
+        $_SESSION['userToAdd']['nombre'] = $_POST['name'];
+        $_SESSION['userToAdd']['apellido1'] = $_POST['secondName'];
+        $_SESSION['userToAdd']['apellido2'] = $_POST['thirdName'];
+        $_SESSION['userToAdd']['fechaDeNacimiento'] = $_POST['birthday'];
+        $_SESSION['userToAdd']['mail'] = $_POST['mail'];
+        header("Location: register2.php");
+    }else{
+        $_SESSION['error']['mailRepeated'] = true;
     }
     
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,73 +50,79 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&family=Quicksand:wght@300&family=Satisfy&display=swap" rel="stylesheet">
     <script src="js/register.js" defer></script>
 </head>
+
 <body>
-    
-        <div class="contenedor">
-            <form action="register2.php" id="userFrom" method="POST">
-                <div class="titulo">
-                    <h1>EASY TRAVEL</h1>
-                </div>
 
-                <div class="titulo2">
-                    <h2>Make your travel easy</h2>
-                </div>
-
-                <div class="texto">
-                    <p class="bold">Bienvenido a EASY TRAVEL</p>
-                    <p>Por favor, rellena los siguientes datos para poder registrarte</p>
-                </div>
-
-                    <div class="campo user1">
-                        <img src="img/user icon.svg" alt="user icon " class="userIcon primero">
-                        <label for="name">INSERTA TU NOMBRE:</label> <br>
-                        <input type="text" name="name" id="name">
-                    </div>
-                
-                <div class="campo">
-                    <img src="img/user icon.svg" alt="user icon" class="userIcon">
-                    <label for="secondName">INSERTA TU PRIMER APELLIDO:</label> <br>
-                    <input type="text" name="secondName" id="secondName">
-                </div>
-
-                <div class="campo">
-                    <img src="img/user icon.svg" alt="user icon" class="userIcon tercero">
-                    <label for="thirdName" class="second-name">INSERTA TU SEGUNDO APELLIDO:</label> <br>
-                    <input type="text" name="thirdName" id="thirdName">
-                </div>
-
-                <div class="campo">
-                    <img src="img/user icon.svg" alt="user icon" class="userIcon tercero">
-                    <label for="mail" class="second-name">INSERTA TU CORREO ELECTRONICO:</label> <br>
-                    <input type="mail" name="mail" id="mail">
-                </div>
-
-                <div class="campo">
-                    <img src="img/" alt="">
-                    <label for="birthday" class="text-birthday">INSERTA TU FECHA DE NACIMIENTO:</label><br>
-                    <input type="date" name="birthday" id="birthday">
-                </div>
-
-                <div class="enviar">
-                    <input class="submit "type="submit" name="nextButton" value="Siguiente">
-                </div>
-            </form>
-        </div>
-
-        <div class="cookies">
-            <div class="cookies_p">
-                <p>
-                    Utilizamos cookies propias y de terceros durante la navegación por el sitio web, con la finalidad de permitir el acceso a las funcionalidades de la página web, extraer estadísticas de tráfico y mejorar la experiencia del usuario. Puedes aceptar todas las cookies, así como seleccionar cuáles deseas habilitar o configurar sus preferencias. Para más información, puede consultar nuestra <a href="#">Política de cookies</a>
-                </p>
+    <div class="contenedor">
+        <form id="userFrom" method="POST">
+            <div class="titulo">
+                <h1>EASY TRAVEL</h1>
             </div>
 
-            <div class="cookies_aceptarRechazar">
-                <button>Aceptar</button>
-                <button>Aceptar mínimos</button>
+            <div class="titulo2">
+                <h2>Make your travel easy</h2>
             </div>
 
+            <div class="texto">
+                <p class="bold">Bienvenido a EASY TRAVEL</p>
+                <p>Por favor, rellena los siguientes datos para poder registrarte</p>
+            </div>
 
+            <div class="campo user1">
+                <img src="img/user icon.svg" alt="user icon " class="userIcon primero">
+                <label for="name">INSERTA TU NOMBRE:</label> <br>
+                <input type="text" name="name" id="name">
+            </div>
+
+            <div class="campo">
+                <img src="img/user icon.svg" alt="user icon" class="userIcon">
+                <label for="secondName">INSERTA TU PRIMER APELLIDO:</label> <br>
+                <input type="text" name="secondName" id="secondName">
+            </div>
+
+            <div class="campo">
+                <img src="img/user icon.svg" alt="user icon" class="userIcon tercero">
+                <label for="thirdName" class="second-name">INSERTA TU SEGUNDO APELLIDO:</label> <br>
+                <input type="text" name="thirdName" id="thirdName">
+            </div>
+
+            <div class="campo">
+                <img src="img/user icon.svg" alt="user icon" class="userIcon tercero">
+                <label for="mail" class="second-name">INSERTA TU CORREO ELECTRONICO:</label> <br>
+                <input type="mail" name="mail" id="mail">
+                <?php 
+                if(isset($_SESSION['error']['mailRepeated']) && $_SESSION['error']['mailRepeated']){
+                    echo "<h4>Este mail ya esta registrado</h4> ";
+                }
+                ?>
+            </div>
+
+            <div class="campo">
+                <img src="img/" alt="">
+                <label for="birthday" class="text-birthday">INSERTA TU FECHA DE NACIMIENTO:</label><br>
+                <input type="date" name="birthday" id="birthday">
+            </div>
+
+            <div class="enviar">
+                <input class="submit " type="submit" name="nextButton" value="Siguiente">
+            </div>
+        </form>
+    </div>
+
+    <div class="cookies">
+        <div class="cookies_p">
+            <p>
+                Utilizamos cookies propias y de terceros durante la navegación por el sitio web, con la finalidad de permitir el acceso a las funcionalidades de la página web, extraer estadísticas de tráfico y mejorar la experiencia del usuario. Puedes aceptar todas las cookies, así como seleccionar cuáles deseas habilitar o configurar sus preferencias. Para más información, puede consultar nuestra <a href="#">Política de cookies</a>
+            </p>
         </div>
-        
-   
+
+        <div class="cookies_aceptarRechazar">
+            <button>Aceptar</button>
+            <button>Aceptar mínimos</button>
+        </div>
+
+
+    </div>
+
+
 </body>
